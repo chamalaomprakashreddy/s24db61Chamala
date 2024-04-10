b2a831d9f0497eb4900a5bcac0057921e38e4212
chamalaomprakashreddy <146696289+chamalaomprakashreddy@users.noreply.github.com>
2024-04-09 23:43:51 -0500

    12

--- models/watercraft.js
+++ models/watercraft.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const watercraftSchema = mongoose.Schema({
 watercraft_type: String,
 length: Number,
 maximum_capacity: Number
-})
+});
 module.exports = mongoose.model("watercraft",
-watercraftSchema)
+watercraftSchema);
