547e34f00cadaf51fd6b954d261809dade48c191
chamalaomprakashreddy <146696289+chamalaomprakashreddy@users.noreply.github.com>
2024-04-11 23:24:43 -0500

    buttons for update

--- controllers/watercraft.js
+++ controllers/watercraft.js
@@ -40,65 +40,11 @@ res.send(`{"error": ${err}}`);
 exports.watercraft_view_all_Page = async function(req, res) {
 try{
 thewatercraft = await watercraft.find();
-res.render('watercraft', { title: 'thewatercraft Search Results', results: watercraft });
+res.render('watercraft', { title: 'watercraft Search Results', results: thewatercraft });
 }
 catch(err){
 res.status(500);
 res.send(`{"error": ${err}}`);
-}
-};
-
-// Handle watercraft create on POST.
-exports.watercraft_create_post = async function(req, res) {
-    console.log(req.body)
-    let document = new watercraft();
-    // We are looking for a body, since POST does not have query parameters.
-    // Even though bodies can be in many different formats, we will be picky
-    // and require that it be a json object
-    // {"watercraft_type":"goat", "cost":12, "size":"large"}
-    document.watercraft_type = req.body.watercraft_type;
-    document.length = req.body.length;
-    document.maximum_capacity = req.body.maximum_capacity;
-    try{
-    let result = await document.save();
-    res.send(result);
-    }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    }
-    };
-
-// for a specific watercraft.
-exports.watercraft_detail = async function(req, res) {
-console.log("detail" + req.params.id)
-try {
-result = await watercraft.findById( req.params.id)
-res.send(result)
-} catch (error) {
-res.status(500)
-res.send(`{"error": document for id ${req.params.id} not found`);
-}
-};
-
-// Handle watercraft update form on PUT.
-exports.watercraft_update_put = async function(req, res) {
-console.log(`update on id ${req.params.id} with body
-${JSON.stringify(req.body)}`)
-try {
-let toUpdate = await watercraft.findById( req.params.id)
-// Do updates of properties
-if(req.body.watercraft_type)
-toUpdate.watercraft_type = req.body.watercraft_type;
-if(req.body.length) toUpdate.length = req.body.length;
-if(req.body.maximum_capacity) toUpdate.maximum_capacity = req.body.maximum_capacity;
-let result = await toUpdate.save();
-console.log("Sucess " + result)
-res.send(result)
-} catch (err) {
-res.status(500)
-res.send(`{"error": ${err}: Update for id ${req.params.id}
-failed`);
 }
 };  
 
