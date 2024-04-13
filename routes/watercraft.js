4c48abec9c4e8df8fe7904ef50fd28f6d54eecf6
chamalaomprakashreddy <146696289+chamalaomprakashreddy@users.noreply.github.com>
2024-04-12 22:14:05 -0500

    added

--- routes/watercraft.js
+++ routes/watercraft.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('watercraft', { title: 'Search Result watercraft' });
@@ -8,7 +8,6 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
-var express = require('express');
 const watercraft_controlers= require('../controllers/watercraft');
 var router = express.Router();
 /* GET watercraft */
@@ -16,7 +15,7 @@ router.get('/', watercraft_controlers.watercraft_view_all_Page );
 module.exports = router;
 
 // GET request for one watercraft.
-//router.get('/watercraft/:id', watercraft_controller.watercraft_detail);
+router.get('/watercraft/:id', watercraft_controlers.watercraft_detail);
 
 /* GET detail watercraft page */
 router.get('/detail', watercraft_controlers.watercraft_view_one_Page);
@@ -24,8 +23,28 @@ router.get('/detail', watercraft_controlers.watercraft_view_one_Page);
 /* GET create watercraft page */
 router.get('/create', watercraft_controlers.watercraft_create_Page);
 
-/* GET create update page */
-router.get('/update', watercraft_controlers.watercraft_update_Page);
+// /* GET create update page */
+// router.get('/update', watercraft_controlers.watercraft_update_Page);
 
+// /* GET delete watercraft page */
+// router.get('/delete', watercraft_controlers.watercraft_delete_Page);
+
+// A little function to check if we have an authorized user and continue on
+
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
+/* GET update watercraft page */
+router.get('/update', secured,watercraft_controlers.watercraft_update_Page);
 /* GET delete watercraft page */
 router.get('/delete', watercraft_controlers.watercraft_delete_Page);
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+  });
+module.exports=router;
